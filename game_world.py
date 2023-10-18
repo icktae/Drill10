91c8388cba95c94f5b9b50b974df10f739423afe
icktae <taechil77@naver.com>
2023-10-18 18:34:11 +0900

    first commit

---
+++ game_world.py
@@ -0,0 +1,26 @@
+objects = [[], []]
+
+
+def add_object(o, depth = 0):
+    objects[depth].append(o)
+
+def update():
+    for layer in objects:
+        for o in layer:
+            o.update()
+
+
+def remder():
+    for layer in objects:
+        for o in layer:
+            o.draw()
+
+
+def remove_object(o):
+    for layer in objects:
+        if o in layer :
+            layer.remove(o)
+            return
+
+    raise ValueError('존재하지 않는 객체 지우지 못함')
+
